df1e095c0b1de501b31239a895d7f3ceb5ffa838
William Staker <123405116+wtstaker@users.noreply.github.com>
2024-12-11 19:15:23 -0700

    Update switches.h

--- project/switches.h
+++ project/switches.h
@@ -1,1 +1,13 @@
+#ifndef switches_included
+#define switches_included
 
+#define SW1 BIT0 //1 is at p2.0
+#define SW2 BIT1 //2 is at p2.1
+#define SW3 BIT2 //3 is at p2.3
+#define SW4 BIT3 //4 is at p2.3
+
+#define SWITCHES (SW1 | SW2 | SW3 | SW4) //all port 2 switches
+
+void switchInit();
+
+#endif // included
