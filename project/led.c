190d9cf297d179159200764975031c2ac44653b4
William Staker <123405116+wtstaker@users.noreply.github.com>
2024-12-11 18:40:06 -0700

    Create led.c

---
+++ project/led.c
@@ -0,0 +1,17 @@
+#include "led.h"
+#include <msp430.h>
+
+//initialize the LED pins as outputs
+void init_leds(void) {
+    P1DIR |= LEDS; // Set P1.0 and P1.6 as outputs
+}
+//turn on the green LED and turn off the red LED
+void turn_on_green_led(void) {
+    P1OUT |= LED_GREEN;
+    P1OUT &= ~LED_RED;
+}
+//turn on the red LED and turn off the green LED
+void turn_on_red_led(void) {
+    P1OUT |= LED_RED;
+    P1OUT &= ~LED_GREEN;
+}
