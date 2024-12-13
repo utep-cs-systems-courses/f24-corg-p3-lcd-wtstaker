47cdb86d0ea8e4e134ea57d7a25ffba1bfcc1be2
William Staker <123405116+wtstaker@users.noreply.github.com>
2024-12-12 17:51:21 -0700

    Update freqAssembly.s

--- project/freqAssembly.s
+++ project/freqAssembly.s
@@ -1,4 +1,4 @@
-// Set initial buzzer frequency
+
 buzzer_set_frequency:
     PUSH    R4                    ; Save registers
     MOV     #1000, R4             ; Load initial frequency into R4
@@ -6,7 +6,7 @@ buzzer_set_frequency:
     MOV     R4, &buzzer_frequency ; Store adjusted frequency in global variable
     POP     R4                    ; Restore registers
     RET                           ; Return to the C program
-//Adjust the frequency  
+ 
 adjust_buzzer_frequency:
     PUSH    R4                    ; Save registers
     ADD     #100, R4              ; Increment frequency by 100
