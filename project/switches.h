aeb4074a1d017a51e85ee619fba26a561dcec644
William Staker <123405116+wtstaker@users.noreply.github.com>
2024-12-12 17:36:33 -0700

    Update switches.h add int handler

--- project/switches.h
+++ project/switches.h
@@ -5,8 +5,9 @@
 #define SW2 BIT1 //2 is at p2.1
 #define SW3 BIT2 //3 is at p2.3
 #define SW4 BIT3 //4 is at p2.3
-
 #define SWITCHES (SW1 | SW2 | SW3 | SW4) //all port 2 switches
+
+void switch_interrupt_handler();
 
 void switchInit();
 
