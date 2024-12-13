a2b4cddb1a292014b4cddfbbf926fa2bad088efb
William Staker <123405116+wtstaker@users.noreply.github.com>
2024-12-12 17:35:28 -0700

    Update switches.c interupts

--- project/switches.c
+++ project/switches.c
@@ -1,5 +1,6 @@
 #include <msp430.h>
 #include "switches.h"
+
 void switchInit(){
   P2REN |= SWITCHES;  
   P2IE |= SWITCHES;   
@@ -7,3 +8,13 @@ void switchInit(){
   P2DIR &= ~SWITCHES; 
   
 }
+void switch_interrupt_handler() {
+    char p2val = P2IN;             // current state of Port 2
+    //update state machine or variables
+    if (!(p2val & SW1)) {          // switch 1: clear screen
+        clearScreen();
+    }
+    if (!(p2val & SW2)) {          // switch 2:
+        
+    }
+    
