121ee603b50f48da4a781fbb33a6e828a9dad56e
William Staker <123405116+wtstaker@users.noreply.github.com>
2024-12-12 17:46:15 -0700

    Update switches.c

--- project/switches.c
+++ project/switches.c
@@ -1,6 +1,9 @@
 #include <msp430.h>
 #include "switches.h"
+#include "stateMachine.h"
+#include "lcdutils.h"
 #include "lcddraw.h"
+
 
 void switchInit(){
   P2REN |= SWITCHES;  
@@ -12,10 +15,12 @@ void switchInit(){
 void switch_interrupt_handler() {
     char p2val = P2IN;             // current state of Port 2
     //update state machine or variables
-    if (!(p2val & SW1)) {          // switch 1: clear screen
-        clearScreen(COLOR_BLUE);
+    if (!(p2val & SW1)) {          // switch 1: advance state
+        advance_state();
     }
-    if (!(p2val & SW2)) {          // switch 2:
-        
+    if (!(p2val & SW2)) {          // switch 2: display message
+        clearScreen(COLOR_BLUE);                      // Clear the screen
+        drawString8x12(20, 20, "Switch 2 Pressed!",   // Display a message
+                   COLOR_WHITE, COLOR_BLACK);
     }
     
